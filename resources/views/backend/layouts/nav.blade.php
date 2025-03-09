477803d0fa931ec35306c126a5ef8dd9dbabd396
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-03-09 11:36:53 +0300

    Refactored backend layout view

--- resources/views/backend/layouts/nav.blade.php
+++ resources/views/backend/layouts/nav.blade.php
@@ -4,13 +4,15 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{ route('home') }}" class="nav-link">Site</a>
-        </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('home') }}" role="button">
+                <i class="fas fa-globe"></i>
+            </a>
+        </li>
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
