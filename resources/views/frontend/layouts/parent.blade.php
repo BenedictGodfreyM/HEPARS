d400cbf46f38faa7242f205df64f236e7fc9ad9b
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-03-09 12:01:47 +0300

    Refactored frontend layout view

--- resources/views/frontend/layouts/parent.blade.php
+++ resources/views/frontend/layouts/parent.blade.php
@@ -6,3 +6,21 @@
     @include('frontend.layouts.nav')
 @endsection
 
+@section('page_content_wrapper')
+<div class="content-wrapper" style="min-height: 632.4px;">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">@yield('page_content_title')</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container">@yield('page_content')</div>
+    </div>
+</div>
+@endsection
+
