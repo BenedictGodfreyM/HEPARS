09c834d47539b7a7151b017788de0877d7ea7914
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-13 14:32:56 +0300

    Refactored backend layout view

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -17,9 +17,9 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('career_paths') }}" class="nav-link {{(request()->is('auth/career_paths') || request()->is('auth/career_paths/*')) ? 'active' : ''}}">
+                    <a href="{{ route('disciplines') }}" class="nav-link {{(request()->is('auth/disciplines') || request()->is('auth/disciplines/*')) ? 'active' : ''}}">
                         <i class="nav-icon fas fa-graduation-cap"></i>
-                        <p>Career Paths</p>
+                        <p>Disciplines</p>
                     </a>
                 </li>
                 <li class="nav-item">
