956ba640195f8a584d3737278a5fb79c9cbdc195
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-15 14:45:44 +0300

    Refactored Dashboard Module

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -12,7 +12,7 @@
             <livewire:shared.stat-box count="{{ $total_programs }}" label="Programs" referral_route="institutions" css_class="bg-info" icon="icon-bag" />
         </div>
         <div class="col-lg-3 col-6">
-            <livewire:shared.stat-box count="{{ $total_careers }}" label="Careers" referral_route="disciplines" css_class="bg-success" icon="icon-bag" />
+            <livewire:shared.stat-box count="{{ $total_careers }}" label="Careers" referral_route="fields" css_class="bg-success" icon="icon-bag" />
         </div>
         <div class="col-lg-3 col-6">
             <livewire:shared.stat-box count="{{ $total_users }}" label="Users" referral_route="dashboard" css_class="bg-warning" icon="icon-bag" />
