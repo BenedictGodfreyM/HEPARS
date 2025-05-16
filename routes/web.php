c9ac01a9fe9feecf161d731bae5e8ee3fc7e040f
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-16 21:58:21 +0300

    Removed unused view routes

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,6 @@ Route::prefix('auth')->group(function () {
 
     Route::prefix('combinations')->group(function () {
 
-        Route::prefix('{combination_id}')->group(function () {
-
-            Route::get('/edit', [App\Http\Controllers\Backend\CombinationController::class, 'edit'])->name('combinations.edit');
-
-        });
-
         Route::get('/register', [App\Http\Controllers\Backend\CombinationController::class, 'register'])->name('combinations.register');
         
         Route::get('/', [App\Http\Controllers\Backend\CombinationController::class, 'index'])->name('combinations');
@@ -24,19 +18,11 @@ Route::prefix('auth')->group(function () {
 
             Route::prefix('careers')->group(function () {
 
-                Route::prefix('{career_id}')->group(function () {
-
-                    Route::get('/edit', [App\Http\Controllers\Backend\CareerController::class, 'edit'])->name('fields.careers.edit');
-
-                });
-
                 Route::get('/register', [App\Http\Controllers\Backend\CareerController::class, 'register'])->name('fields.careers.register');
                 
                 Route::get('/', [App\Http\Controllers\Backend\CareerController::class, 'index'])->name('fields.careers');
 
             });
-
-            Route::get('/edit', [App\Http\Controllers\Backend\FieldController::class, 'edit'])->name('fields.edit');
 
         });
 
@@ -51,22 +37,12 @@ Route::prefix('auth')->group(function () {
         Route::prefix('{institution_id}')->group(function () {
 
             Route::prefix('programs')->group(function () {
-
-                Route::prefix('{program_id}')->group(function () {
-
-                    Route::get('/edit', [App\Http\Controllers\Backend\ProgramController::class, 'edit'])->name('institutions.programs.edit');
-                    
-                    Route::get('/show', [App\Http\Controllers\Backend\ProgramController::class, 'show'])->name('institutions.programs.show');
-                
-                });
                     
                 Route::get('/register', [App\Http\Controllers\Backend\ProgramController::class, 'register'])->name('institutions.programs.register');
 
                 Route::get('/', [App\Http\Controllers\Backend\ProgramController::class, 'index'])->name('institutions.programs');
             
             });
-
-            Route::get('/edit', [App\Http\Controllers\Backend\InstitutionController::class, 'edit'])->name('institutions.edit');
 
         });
 
@@ -77,12 +53,6 @@ Route::prefix('auth')->group(function () {
     });
 
     Route::prefix('subjects')->group(function () {
-
-        Route::prefix('{subject_id}')->group(function () {
-
-            Route::get('/edit', [App\Http\Controllers\Backend\SubjectController::class, 'edit'])->name('subjects.edit');
-
-        });
 
         Route::get('/register', [App\Http\Controllers\Backend\SubjectController::class, 'register'])->name('subjects.register');
         
