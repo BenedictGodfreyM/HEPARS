91203c158f3107971ef68e544a69483c101b4ad0
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-17 19:46:48 +0300

    Refactored Database setup

---
+++ database/migrations/2025_05_17_052535_add_affiliation_id_to_institutions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('institutions', function (Blueprint $table) {
+            $table->string('affiliation_id')->index()->nullable();
+            $table->foreign('affiliation_id')->references('id')->on('institutions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('institutions', function (Blueprint $table) {
+            $table->dropColumn('affiliation_id');
+        });
+    }
+};
