b44102647ab63782d9371195ec6fd44d60251f61
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-17 19:48:17 +0300

    Refactored data access logic for institution

--- app/Models/Institution.php
+++ app/Models/Institution.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Institution extends Model
 {
@@ -30,6 +33,7 @@ class Institution extends Model
      * @var list<string>
      */
     protected $fillable = [
+        'affiliation_id',
         'name',
         'acronym',
         'type',
@@ -40,8 +44,65 @@ class Institution extends Model
         'rank'
     ];
 
+    /**
+     * Get the institution's accreditation status (Eg. Accredited and Chartered, Accredited, e.t.c)
+     * 
+     * @return string
+     */
+    public function getAccreditationStatusAttribute(): string
+    {
+        $items = $this->accreditations->sortBy('status')->pluck('status')->toArray();
+        $items = array_values($items);
+        $count = count($items);
+        if ($count === 0) return 'N/A';
+        if ($count === 1) return "{$items[0]}";
+        if ($count === 2) return "{$items[0]} and {$items[1]}";
+
+        return implode(', ', array_slice($items, 0, -1)) . " and " . end($items);
+    }
+
+    /**
+     * Get all mother institutions.
+     * 
+     * @param Illuminate\Database\Eloquent\Builder $query
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeMotherInstitutions(Builder $query): Builder
+    {
+        return $query->whereNull('affiliation_id');
+    }
+
+    /**
+     * Get all mother institutions except a certain institution.
+     * 
+     * @param Illuminate\Database\Eloquent\Builder $query
+     * @param string $institution_id An Institution to exclude from the list
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeMotherInstitutionsExcept(Builder $query, string $institution_id): Builder
+    {
+        return $query->whereNull('affiliation_id')->where('id', '<>', $institution_id);
+    }
+    
+    public function affiliates(): HasMany
+    {
+        return $this->hasMany(Institution::class, 'affiliation_id');
+    }
+    
+    public function affiliatedTo(): BelongsTo
+    {
+        return $this->belongsTo(Institution::class, 'affiliation_id');
+    }
+
     public function programs(): HasMany
     {
         return $this->hasMany(Program::class);
+    }
+
+    public function accreditations(): BelongsToMany
+    {
+        return $this->belongsToMany(Accreditation::class, 'accreditation_institutions');
     }
 }
--- app/Repositories/InstitutionRepository.php
+++ app/Repositories/InstitutionRepository.php
@@ -35,6 +35,38 @@ class InstitutionRepository
     }
 
     /**
+     * Retrieve all institutions from the database (Without Pagination)
+     * 
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function allInstitutionsWithoutPagination()
+    {
+        return Institution::query()->orderBy('name','asc')->get();
+    }
+
+    /**
+     * Retrieve all mother institutions from the database (Without Pagination)
+     * 
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function allMotherInstitutionsWithoutPagination()
+    {
+        return Institution::query()->motherInstitutions()->orderBy('name','asc')->get();
+    }
+
+    /**
+     * Retrieve all mother institutions from the database except a certain institution (Without Pagination)
+     * 
+     * @param string $institution_id An Institution to exclude from the list
+     * 
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function allMotherInstitutionsExceptWithoutPagination(string $institution_id)
+    {
+        return Institution::query()->motherInstitutionsExcept($institution_id)->orderBy('name','asc')->get();
+    }
+
+    /**
      * Save details of an institution to the database
      * 
      * @param array $data Details of a particular institution
@@ -44,6 +76,7 @@ class InstitutionRepository
     public function storeInstitution(array $data)
     {
         return Institution::create([
+            'affiliation_id' => $data['affiliation_id'],
             'name' => $data['name'],
             'acronym' => $data['acronym'],
             'type' => $data['type'],
@@ -78,6 +111,7 @@ class InstitutionRepository
     public function updateInstitution(array $data, string $id)
     {
         $institution = Institution::where('id', $id)->firstOrFail();
+        $institution->affiliation_id = $data['affiliation_id'];
         $institution->name = $data['name'];
         $institution->acronym = $data['acronym'];
         $institution->type = $data['type'];
@@ -117,5 +151,33 @@ class InstitutionRepository
             'name' => $data['name'],
             'duration' => $data['duration'],
         ]);
+    }
+
+    /**
+     * Link a particular institution to a specific accreditation
+     * 
+     * @param string $institution_id
+     * @param string $accreditation_id
+     * 
+     * @return void
+     */
+    public function linkToAccreditation(string $institution_id, string $accreditation_id)
+    {
+        $institution = Institution::findOrFail($institution_id);
+        $institution->accreditations()->attach($accreditation_id);
+    }
+
+    /**
+     * Synchronize a particular institution to multiple accreditations
+     * 
+     * @param string $institution_id
+     * @param array $accreditations Array of IDs of specific accreditations
+     * 
+     * @return void
+     */
+    public function linkToAccreditations(string $institution_id, array $accreditations)
+    {
+        $institution = Institution::findOrFail($institution_id);
+        $institution->accreditations()->sync($accreditations);
     }
 }
