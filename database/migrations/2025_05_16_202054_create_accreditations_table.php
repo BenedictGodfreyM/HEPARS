bf2a3eda21befa4519fc789e69f1e8fae097d60e
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-17 14:51:51 +0300

    Refactored Database setup

---
+++ database/migrations/2025_05_16_202054_create_accreditations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accreditations', function (Blueprint $table) {
+            $table->engine = "InnoDB";
+            $table->string('id')->primary();
+            $table->string('status');
+            $table->integer('rating', false, true);
+            $table->mediumText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accreditations');
+    }
+};
---
+++ database/migrations/2025_05_16_275211_create_accreditation_institutions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accreditation_institutions', function (Blueprint $table) {
+            $table->engine = "InnoDB";
+            $table->bigIncrements('id');
+            $table->string('accreditation_id')->index();
+            $table->foreign('accreditation_id')->references('id')->on('accreditations')->onDelete('cascade');
+            $table->string('institution_id')->index();
+            $table->foreign('institution_id')->references('id')->on('institutions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accreditation_institutions');
+    }
+};
