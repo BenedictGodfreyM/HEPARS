e1fca7a960de541c750992f9d57a3ad5847056a7
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-28 19:20:13 +0300

    Refactored frontend layout view

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -22,11 +22,19 @@
 
         <!-- Right navbar links -->
         <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+            @auth
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('dashboard') }}" role="button">
                     <i class="fas fa-user"></i>
                 </a>
             </li>
+            @else
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}" role="button">
+                    <i class="fas fa-sign-in-alt"></i>
+                </a>
+            </li>
+            @endauth
             <li class="nav-item">
                 <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                     <i class="fas fa-expand-arrows-alt"></i>
