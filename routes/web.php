42685671eb03c1e4d6ffd32b354ccbbaefca6c7b
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-05-28 19:03:32 +0300

    Added view routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
+Route::middleware('auth')->prefix('auth')->group(function () {
+
+    Route::prefix('account')->group(function () {
+
+        Route::get('/', [App\Http\Controllers\Backend\UserAccountController::class, 'index'])->name('account.details');
+
+    });
 
     Route::prefix('accreditation')->group(function () {
 
@@ -68,7 +74,50 @@ Route::prefix('auth')->group(function () {
 
     });
 
+    Route::prefix('verify-email')->group(function () {
+
+        Route::get('/', App\Http\Controllers\Auth\EmailVerificationPromptController::class)->name('verification.notice');
+
+        Route::get('/{id}/{hash}', App\Http\Controllers\Auth\VerifyEmailController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+
+    });
+
+    Route::post('email/verification-notification', [App\Http\Controllers\Auth\EmailVerificationNotificationController::class, 'store'])->middleware('throttle:6,1')->name('verification.send');
+
+
+    Route::prefix('confirm-password')->group(function () {
+
+        Route::get('/', [App\Http\Controllers\Auth\ConfirmablePasswordController::class, 'show'])->name('password.confirm');
+
+        Route::post('/', [App\Http\Controllers\Auth\ConfirmablePasswordController::class, 'store']);
+
+    });
+
+    Route::put('password', [App\Http\Controllers\Auth\PasswordController::class, 'update'])->name('password.update');
+
+    Route::post('logout', [App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
     Route::get('/', [App\Http\Controllers\Backend\DashboardController::class, 'index'])->name('dashboard');
+
+});
+
+Route::middleware('guest')->group(function () {
+
+    Route::get('register', [App\Http\Controllers\Auth\RegisteredUserController::class, 'create'])->name('register');
+
+    Route::post('register', [App\Http\Controllers\Auth\RegisteredUserController::class, 'store']);
+
+    Route::get('login', [App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'create'])->name('login');
+
+    Route::post('login', [App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store']);
+
+    Route::get('forgot-password', [App\Http\Controllers\Auth\PasswordResetLinkController::class, 'create'])->name('password.request');
+
+    Route::post('forgot-password', [App\Http\Controllers\Auth\PasswordResetLinkController::class, 'store'])->name('password.email');
+
+    Route::get('reset-password/{token}', [App\Http\Controllers\Auth\NewPasswordController::class, 'create'])->name('password.reset');
+
+    Route::post('reset-password', [App\Http\Controllers\Auth\NewPasswordController::class, 'store'])->name('password.store');
 
 });
 
