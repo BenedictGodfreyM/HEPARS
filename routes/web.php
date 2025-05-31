11ef5f4681d73e06fc757fcb350243ad48dcab7f
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-06-01 00:07:05 +0300

    Refactored view routes

--- routes/web.php
+++ routes/web.php
@@ -4,75 +4,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->prefix('auth')->group(function () {
 
-    Route::prefix('account')->group(function () {
+    Route::get('/account', [App\Http\Controllers\Backend\UserAccountController::class, 'index'])->name('account.details');
 
-        Route::get('/', [App\Http\Controllers\Backend\UserAccountController::class, 'index'])->name('account.details');
+    
+    Route::get('/accreditation', [App\Http\Controllers\Backend\AccreditationController::class, 'index'])->name('accreditation');
 
-    });
 
-    Route::prefix('accreditation')->group(function () {
+    Route::get('/combinations', [App\Http\Controllers\Backend\CombinationController::class, 'index'])->name('combinations');
 
-        Route::get('/register', [App\Http\Controllers\Backend\AccreditationController::class, 'register'])->name('accreditation.register');
-        
-        Route::get('/', [App\Http\Controllers\Backend\AccreditationController::class, 'index'])->name('accreditation');
-
-    });
-
-    Route::prefix('combinations')->group(function () {
-
-        Route::get('/register', [App\Http\Controllers\Backend\CombinationController::class, 'register'])->name('combinations.register');
-        
-        Route::get('/', [App\Http\Controllers\Backend\CombinationController::class, 'index'])->name('combinations');
-
-    });
 
     Route::prefix('fields')->group(function () {
 
-        Route::prefix('{field_id}')->group(function () {
+        Route::get('/{field_id}/careers', [App\Http\Controllers\Backend\CareerController::class, 'index'])->name('fields.careers');
 
-            Route::prefix('careers')->group(function () {
-
-                Route::get('/register', [App\Http\Controllers\Backend\CareerController::class, 'register'])->name('fields.careers.register');
-                
-                Route::get('/', [App\Http\Controllers\Backend\CareerController::class, 'index'])->name('fields.careers');
-
-            });
-
-        });
-
-        Route::get('/register', [App\Http\Controllers\Backend\FieldController::class, 'register'])->name('fields.register');
-        
         Route::get('/', [App\Http\Controllers\Backend\FieldController::class, 'index'])->name('fields');
 
     });
 
     Route::prefix('institutions')->group(function () {
 
-        Route::prefix('{institution_id}')->group(function () {
-
-            Route::prefix('programs')->group(function () {
-                    
-                Route::get('/register', [App\Http\Controllers\Backend\ProgramController::class, 'register'])->name('institutions.programs.register');
-
-                Route::get('/', [App\Http\Controllers\Backend\ProgramController::class, 'index'])->name('institutions.programs');
-            
-            });
-
-        });
-
-        Route::get('/register', [App\Http\Controllers\Backend\InstitutionController::class, 'register'])->name('institutions.register');
+        Route::get('/{institution_id}/programs', [App\Http\Controllers\Backend\ProgramController::class, 'index'])->name('institutions.programs');  
 
         Route::get('/', [App\Http\Controllers\Backend\InstitutionController::class, 'index'])->name('institutions');
 
     });
+    
+    Route::get('/subjects', [App\Http\Controllers\Backend\SubjectController::class, 'index'])->name('subjects');
 
-    Route::prefix('subjects')->group(function () {
-
-        Route::get('/register', [App\Http\Controllers\Backend\SubjectController::class, 'register'])->name('subjects.register');
-        
-        Route::get('/', [App\Http\Controllers\Backend\SubjectController::class, 'index'])->name('subjects');
-
-    });
 
     Route::prefix('verify-email')->group(function () {
 
