23db54f50052543d56cc1eb76801ceb0f5686361
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-06-01 00:49:48 +0300

    Refactored backend layout view
    Refactored backend layout view

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,7 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ route('dashboard') }}" class="brand-link">
-        <img src="{{ asset('assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="{{ route('dashboard') }}" class="brand-link text-center">
         <span class="brand-text font-weight-light">HEPARS</span>
     </a>
 
