831a1e7f1c85b99674b8fef000521e09b2eba364
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-06-05 00:12:15 +0300

    Refactored Dashboard Module

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -5,17 +5,25 @@
 @section('page_content')
     <!-- Small boxes (Stat box) -->
     <div class="row">
+        @permission('view.institutions')
         <div class="col-lg-3 col-6">
             <livewire:shared.stat-box count="{{ $total_institutions }}" label="Institutions" referral_route="institutions" css_class="bg-olive" icon="icon-bag" />
         </div>
+        @endpermission
+        @permission('view.programs')
         <div class="col-lg-3 col-6">
             <livewire:shared.stat-box count="{{ $total_programs }}" label="Programs" referral_route="institutions" css_class="bg-info" icon="icon-bag" />
         </div>
+        @endpermission
+        @permission('view.careers')
         <div class="col-lg-3 col-6">
             <livewire:shared.stat-box count="{{ $total_careers }}" label="Careers" referral_route="fields" css_class="bg-success" icon="icon-bag" />
         </div>
+        @endpermission
+        @permission('view.users')
         <div class="col-lg-3 col-6">
             <livewire:shared.stat-box count="{{ $total_users }}" label="Users" referral_route="dashboard" css_class="bg-warning" icon="icon-bag" />
         </div>
+        @endpermission
     </div>
 @endsection
