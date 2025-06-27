d119c4b48f114724bf969c8324384c6f83dd762e
Godfrey M. Benedicto <gmashinji@gmail.com>
2025-06-27 12:46:13 +0300

    Refactored the Dashboard Module

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -7,6 +7,8 @@ use App\Repositories\CareerRepository;
 use App\Repositories\InstitutionRepository;
 use App\Repositories\ProgramRepository;
 use App\Repositories\UserRepository;
+use App\Services\RecommendationService;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {    
@@ -14,6 +16,7 @@ class DashboardController extends Controller
     protected $programRepo;
     protected $careerRepo;
     protected $userRepo;
+    protected $recommendationService;
 
     public function __construct()
     {
@@ -21,12 +24,20 @@ class DashboardController extends Controller
         $this->programRepo = new ProgramRepository();
         $this->careerRepo = new CareerRepository();
         $this->userRepo = new UserRepository();
+        $this->recommendationService = new RecommendationService();
     }
 
     public function index()
-    {        return view('backend.dashboard')->with('total_institutions', $this->institutionRepo->totalInstitutions())
+    {     
+        $all_requests = $this->recommendationService->getChartData();
+        $my_requests = $this->recommendationService->getChartData(Auth::user()->id);   
+        return view('backend.dashboard')->with('total_institutions', $this->institutionRepo->totalInstitutions())
                                         ->with('total_programs', $this->programRepo->totalPrograms())
                                         ->with('total_careers', $this->careerRepo->totalCareers())
-                                        ->with('total_users', $this->userRepo->totalUsers());
+                                        ->with('total_users', $this->userRepo->totalUsers())
+                                        ->with('my_recommendation_requests_data', $my_requests['data'])
+                                        ->with('my_recommendation_requests_labels', $my_requests['labels'])
+                                        ->with('all_recommendation_requests_data', $all_requests['data'])
+                                        ->with('all_recommendation_requests_labels', $all_requests['labels']);
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -22,7 +22,37 @@
         @endpermission
         @permission('view.users')
         <div class="col-lg-3 col-6">
-            <livewire:shared.stat-box count="{{ $total_users }}" label="Users" referral_route="dashboard" css_class="bg-warning" icon="icon-bag" />
+            <livewire:shared.stat-box count="{{ $total_users }}" label="Users" referral_route="users" css_class="bg-warning" icon="icon-bag" />
+        </div>
+        @endpermission
+    </div>
+    <div class="row">
+        @permission('view.recommendation.history.chart')
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <i class="fas fa-chart-line mr-1"></i> My Recommendation Request History Stats
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <livewire:shared.line-chart description="Number of Requests" :chartLabels='$my_recommendation_requests_labels' :chartData='$my_recommendation_requests_data' />
+                </div>
+            </div>
+        </div>
+        @endpermission
+        @permission('view.recommendation.history.chart.of.all.users')
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <i class="fas fa-chart-bar mr-1"></i> All Recommendation Request History Stats
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <livewire:shared.bar-chart description="Number of Requests" :chartLabels='$all_recommendation_requests_labels' :chartData='$all_recommendation_requests_data' />
+                </div>
+            </div>
         </div>
         @endpermission
     </div>
